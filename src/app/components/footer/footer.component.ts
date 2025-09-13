9ff60c318d1f1914998d4ff26b28a560042952f3
lunadiatto <luna.diatto@gmail.com>
2025-09-13 22:04:50 +0200

    Footer component static

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../_shared/shared.module';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faLinkedinIn, faGithub, faLink);
+  }
 
 }
