da74d47de49fd2db5763840e1b003f4b2c7aaab5
loryF23 <lory.frontino@gmail.com>
2025-09-22 23:51:56 +0200

    ui component card-user

--- src/app/components/card-user/card-user.ts
+++ src/app/components/card-user/card-user.ts
@@ -1,15 +1,24 @@
 import {Component, OnInit} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import {FaIconComponent, FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {fab, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {SharedModule} from '../../_shared/shared.module';
 
 @Component({
   selector: 'app-card-user',
   imports: [
-    NgOptimizedImage
+    SharedModule,
+    NgOptimizedImage,
+    FaIconComponent
   ],
   templateUrl: './card-user.html',
   styleUrl: './card-user.scss'
 })
 export class CardUser implements OnInit {
+  constructor(library:FaIconLibrary) {
+    library.addIconPacks(fab);
+    library.addIcons(faGithub, faLinkedin);
+  }
   img: string = '';
   ngOnInit() {
 
