96058b7d50285495fcbc6dc941ef092acfbf8369
loryF23 <lory.frontino@gmail.com>
2025-09-23 00:11:13 +0200

    ui for section of speakers and organizers

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -2,20 +2,25 @@ import {Component, OnInit} from '@angular/core';
 import {Headline} from '../../components/headline/headline';
 import {HomeStructureModel} from '../../_shared/models/home.model';
 import {CardUser} from '../../components/card-user/card-user';
+import {FaIconComponent, FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {fab, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   imports: [
     Headline,
-    CardUser
+    CardUser,
+    FaIconComponent
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent implements OnInit{
   homeData: HomeStructureModel = {} as HomeStructureModel;
-  constructor() {
+  constructor(library:FaIconLibrary) {
+    library.addIcons(faChevronRight);
   }
   ngOnInit() {
     // this.homeData.header.main_image.src = 'public/images/back-monti.png';
