77924f45e43b9717de8f507d9c4dc4c522f5cf85
loryF23 <lory.frontino@gmail.com>
2025-09-23 22:03:02 +0200

    ui for call for speakers and call for helpers

--- src/app/components/card-user/card-user.ts
+++ src/app/components/card-user/card-user.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
 import {FaIconComponent, FaIconLibrary} from '@fortawesome/angular-fontawesome';
 import {fab, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
@@ -15,6 +15,7 @@ import {SharedModule} from '../../_shared/shared.module';
   styleUrl: './card-user.scss'
 })
 export class CardUser implements OnInit {
+  @Input() isSpeaker: boolean = false;
   constructor(library:FaIconLibrary) {
     library.addIconPacks(fab);
     library.addIcons(faGithub, faLinkedin);
